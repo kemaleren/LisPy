461e0ef351e1c5ab87adaf572838938da8b440a3
Kemal Eren <kemal.eren.dev@gmail.com>
2010-05-19 20:05:46 -0400

    wrote better exceptions

--- interpretor.py
+++ interpretor.py
@@ -21,13 +21,13 @@ class SExp:
         """
         if right is None:
             if not isinstance(left, str):
-                raise Exception("Not a string")
+                raise Exception("trying to create an S-expression from {0}".format(str(left)))
             if ATOM.match(left) is None:
-                raise Exception("Not an atomic S-expression")
+                raise Exception("not a valid atomic S-expression: {0}".format(left))
             self.val = left
         else:
             if not isinstance(left, SExp) or not isinstance(right, SExp):
-                raise Exception("Not an S-expression")
+                raise Exception("not an S-expression")
             self.val = (left, right)
 
     def _atom(self):
@@ -39,7 +39,8 @@ class SExp:
         return NIL_sexp
 
     def _eq(self, other):
-        if not self._atom() or not other._atom(): raise Exception("Not an atomic S-expression")
+        if not self._atom(): raise Exception("not an atomic S-expression: {0}".format(self))
+        if not other._atom(): raise Exception("not an atomic S-expression: {0}".format(other))
         return self.val == other.val
 
     def eq(self, other):
@@ -64,15 +65,16 @@ class SExp:
         return NIL_sexp
 
     def car(self):
-        if self._atom(): raise Exception("Atomic")
+        if self._atom(): raise Exception("cannot call CAR on atomic s-expression: {0}".format(self))
         return self.val[0]
 
     def cdr(self):
-        if self._atom(): raise Exception("Atomic")
+        if self._atom(): raise Exception("cannot call CDR on atomic s-expression: {0}".format(self))
         return self.val[1]
 
     def _arithmetic(self, other, op):
-        if not self._int() or not other._int(): raise Exception("not an int")
+        if not self._int(): raise Exception("not an int: {0}".format(self))
+        if not other._int(): raise Exception("not an int: {0}".format(other))
         return SExp(str(op(int(self.val),
                            int(other.val))))
 
@@ -92,7 +94,8 @@ class SExp:
         return self._arithmetic(other, lambda a,b: a%b)
 
     def _compare(self, other, op):
-        if not self._int() or not other._int(): raise Exception("not an int")
+        if not self._int(): raise Exception("not an int: {0}".forat(self))
+        if not other._int(): raise Exception("not an int: {0}".format(other))
         if op(int(self.val), int(other.val)): return T_sexp
         return NIL_sexp
         
@@ -149,12 +152,12 @@ def lex(myinput):
 
 def process_list_tokens(tokens):
     """Parses tokens in list form into an s-expression"""
-    if len(tokens) == 0: raise Exception("Parse error: missing tokens")
+    if len(tokens) == 0: raise Exception("parse error: missing tokens")
     if tokens[0] == ")":
         tokens.pop(0)
         return NIL_sexp
     first = process_tokens(tokens)
-    if len(tokens) == 0: raise Exception("Parse error: missing tokens")
+    if len(tokens) == 0: raise Exception("parse error: missing tokens")
     if tokens[0] == ".": raise Exception("mixed notation not supported")
     second = process_list_tokens(tokens)
     return SExp(first, second)
@@ -162,7 +165,7 @@ def process_list_tokens(tokens):
 
 def process_tokens(tokens):
     """Parses tokens into an s-expression"""
-    if len(tokens) == 0: raise Exception("Parse error: missing tokens")
+    if len(tokens) == 0: raise Exception("parse error: missing tokens")
     if ATOM.match(tokens[0]):
         return SExp(tokens.pop(0))
     if tokens[0] == "(" and tokens[1] == ")":
@@ -172,12 +175,12 @@ def process_tokens(tokens):
     #recursively continue
     if not tokens.pop(0) == "(": raise Exception("missing open parentheses")
     first = process_tokens(tokens)
-    if len(tokens) == 0: raise Exception("Parse error: missing tokens")
+    if len(tokens) == 0: raise Exception("parse error: missing tokens")
     second = []
     if tokens[0] == ".":
         tokens.pop(0)
         second = process_tokens(tokens)
-        if len(tokens) == 0: raise Exception("Parse error: missing tokens")
+        if len(tokens) == 0: raise Exception("parse error: missing tokens")
         if not tokens.pop(0) == ")": raise Exception("missing close parentheses")
     else:
         second = process_list_tokens(tokens)
@@ -188,7 +191,7 @@ def parse(myinput):
     """Parses in input and returns it as an s-expression"""
     tokens = [i for i in lex(myinput)]
     sexp = process_tokens(tokens)
-    if len(tokens) > 0: raise Exception("extra tokens found")
+    if len(tokens) > 0: raise Exception("extra tokens found: {0}".format(", ".join(tokens)))
     return sexp
 
 
@@ -207,7 +210,7 @@ def getval(exp, from_list):
 
 
 def addpairs(params, cur_args, to_list):
-    if params._null() and cur_args._null(): return aList
+    if params._null() and cur_args._null(): return to_list
     if params._atom() or cur_args._atom(): raise Exception("pairs cannot be atoms")
     pair = SExp(params.car(), cur_args.car())
     return addpairs(params.cdr(), cur_args.cdr(), SExp(pair, to_list))
@@ -218,7 +221,7 @@ def myeval(exp, aList, dList):
         if exp._int(): return exp
         if exp._eq(T_sexp): return T_sexp
         if exp._null(): return NIL_sexp
-        if in_pairlist(exp, aList): return getVal(exp, alist)
+        if in_pairlist(exp, aList): return getval(exp, aList)
         raise Exception("unbound variable: {0}".format(exp.val))
     if exp.car()._atom():
         if exp.car().val == "QUOTE": return exp.cdr().car()
@@ -227,10 +230,9 @@ def myeval(exp, aList, dList):
             f = exp.cdr().car()
             args = exp.cdr().cdr().car()
             body = exp.cdr().cdr().cdr().car()
-            defun(f, args, body, dList)
-            return
+            return defun(f, args, body, dList)
         return my_apply(exp.car(), evlis(exp.cdr(), aList, dList), aList, dList)
-    raise Exception("error!")
+    raise Exception("eval called with invalid expression")
         
     
 def evlis(targetlist, aList, dList):
@@ -239,7 +241,7 @@ def evlis(targetlist, aList, dList):
                 evlis(targetlist.cdr(), aList, dList))
 
 def my_apply(f, x, aList, dList):
-    if not f._atom(): raise Exception("error: {0} is not a function name".format(f))
+    if not f._atom(): raise Exception("error: {0} is not a known function".format(f))
     if f.val == "CAR": return x.car().car()
     if f.val == "CDR": return x.car().cdr()
     if f.val == "CONS": return SExp(x.car(), x.cdr())
@@ -262,10 +264,11 @@ def my_apply(f, x, aList, dList):
 def defun(f, args, body, dList):
     new_dList = SExp(SExp(f, SExp(args, body)), copy.copy(dList))
     dList.val = new_dList.val
+    return f
     
 
 def evcond(be, aList, dList):
-    if be._null(): raise Exception("error!")
+    if be._null(): raise Exception("boolean expression cannot be NIL")
     if not (myeval(be.car().car(), aList, dList))._null():
         return myeval(be.car().cdr().car(), aList, dList)
     return evcond(be.cdr(), aList, dList)
@@ -273,11 +276,11 @@ def evcond(be, aList, dList):
 
 def interpreter(dList):
     while True:
-        expression = raw_input("LISP > ")
+        expression = raw_input("LISP>")
         try:
             print myeval(parse(expression), NIL_sexp, dList)
         except Exception as inst:
-            print inst.args[0]
+            print "error: " + inst.args[0]
             
 
 if __name__ == "__main__":
